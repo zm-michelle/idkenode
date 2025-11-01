f436dfaa7dd8bf8910f916a19b9e9f98da7d8ba3
Michelle Benites Mendez <mbenites@i56.red.uits.iu.edu>
2025-10-31 20:23:39 -0400

    all

--- test6.sh
+++ test6.sh
@@ -2,19 +2,21 @@
 
 
 
-python /Users/zashabenites/Documents/IU/csciy_390/github_paper/PURE/PURE_ENODE/runner.py --eta_policy 5e-4 --eta_dynamics 5e-8
-python /Users/zashabenites/Documents/IU/csciy_390/github_paper/PURE/PURE_ENODE/runner.py --eta_policy 5e-4 --eta_dynamics 5e-8
-python /Users/zashabenites/Documents/IU/csciy_390/github_paper/PURE/PURE_ENODE/runner.py --eta_policy 5e-4 --eta_dynamics 5e-8
+python runner.py --eta_policy 5e-4 --eta_dynamics 5e-8
+python runner.py --eta_policy 5e-4 --eta_dynamics 5e-8
+python runner.py --eta_policy 5e-5 --eta_dynamics 5e-8
+python runner.py --eta_policy 5e-5 --eta_dynamics 5e-8
 
 
 
-python /Users/zashabenites/Documents/IU/csciy_390/github_paper/PURE/PURE_ENODE/runner.py --eta_policy 1e-4 --eta_dynamics 5e-8 
-python /Users/zashabenites/Documents/IU/csciy_390/github_paper/PURE/PURE_ENODE/runner.py --eta_policy 1e-4 --eta_dynamics 5e-8 
-python /Users/zashabenites/Documents/IU/csciy_390/github_paper/PURE/PURE_ENODE/runner.py --eta_policy 1e-4 --eta_dynamics 5e-8 
+python runner.py --eta_policy 1e-4 --eta_dynamics 5e-8 
+python runner.py --eta_policy 1e-4 --eta_dynamics 5e-8 
+python runner.py --eta_policy 1e-3 --eta_dynamics 5e-8 
 
 
+python /Users/zashabenites/Documents/IU/csciy_390/github_paper/PURE/PURE_ENODE/runner.py --eta_policy 1e-5 --eta_dynamics 5e-8
+python /Users/zashabenites/Documents/IU/csciy_390/github_paper/PURE/PURE_ENODE/runner.py --eta_policy 1e-5 --eta_dynamics 5e-8
 python /Users/zashabenites/Documents/IU/csciy_390/github_paper/PURE/PURE_ENODE/runner.py --eta_policy 1e-3 --eta_dynamics 5e-8
-python /Users/zashabenites/Documents/IU/csciy_390/github_paper/PURE/PURE_ENODE/runner.py --eta_policy 1e-3 --eta_dynamics 5e-8
-python /Users/zashabenites/Documents/IU/csciy_390/github_paper/PURE/PURE_ENODE/runner.py --eta_policy 1e-3 --eta_dynamics 5e-8
+
 
  
